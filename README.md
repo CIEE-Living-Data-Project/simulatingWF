a5f40eaadb4fb2d70b828c4058f39f092d936627
Marc-Olivier Beausoleil <beausoleilmo@gmail.com>
2022-09-07 15:01:12 -0400

    Initialize

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# simulatingWF
-Simulation of the Wright–Fisher Model (genetic drift)
+# Simulating the Wright-Fisher model in R and Hardy-Weinberg principle of genotype frequencies
+
+This repository shows how to simulation of the Wright–Fisher Model (genetic drift) and plot the Hardy-Weinberg principle. 
+
+## Structure of the project
+
+If the folder structure is not created, you should run the `0.initialize.R` script which will install and load all libraries, create the folder structure and print the session info for R (version of packages and `R`).
+
---
+++ scripts/0.initialize.R
@@ -0,0 +1,60 @@
+# Description  ------------------------------------------------------------
+##########################################################################################
+# Initialize the project
+# Created by Marc-Olivier Beausoleil
+# McGill University 
+# Created 2022-09-07
+# Why: 
+# Requires 
+# NOTES: 
+##########################################################################################
+cat("Initializing project and preparing R.\n")
+
+# Load libraries ----------------------------------------------------------
+# if (!"librarian" %in% installed.packages()) {
+#   install.packages("librarian") 
+# } else {
+#   cat("librarian package already installed. Loading package librarian.\n")
+#   library(librarian)
+# }
+# librarian::shelf(tidyverse, cran_repo = 'https://cran.r-project.org')
+if (!"groundhog" %in% installed.packages()) {
+  install.packages("groundhog") 
+} else {
+  cat("groundhog package already installed. Loading package groundhog\n")
+  library(groundhog)
+}
+pkgs = c("tidyverse")
+# Should be at least 2 days in the past Sys.Date()-2
+groundhog.library(pkgs, "2022-09-05")
+
+# Folder structure --------------------------------------------------------
+cat("Creating folders if needed\n")
+if (basename(getwd()) != "report") {
+  folders = c("output","data",
+              "report/references",
+              "report/styles")
+  junk <- mapply(FUN = dir.create, 
+                 path = folders, 
+                 showWarnings = FALSE); rm(list = "junk")
+}
+
+
+# Session info ------------------------------------------------------------
+if (basename(getwd()) != "report") {
+  
+  cat("Generate session information\n")
+  
+  dir.create("output/session_info",showWarnings = FALSE)
+  sink("output/session_info/session_information.txt", append = FALSE)
+  # sink("~/Desktop/session_information.txt",append = FALSE)
+  cat("##### R Version Information ############################################################\n")
+  version
+  
+  cat("\n\n##### Collect Information About the Current R Session ############################################################\n\n")
+  sessionInfo() 
+  # loadedNamespaces()
+  sink()
+}
+cat("Done!\n")
+
